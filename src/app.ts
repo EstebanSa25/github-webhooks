9be1fbe15a276e7de84dea01e3f0835699f3f41c
EstebanSa25 <estebansalas2502@gmail.com>
2024-01-10 16:18:59 -0600

    Fin seccion 24

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware';
 (() => {
     main();
 })();
@@ -9,6 +10,7 @@ async function main() {
     // const service= new GithubService()
     const controller = new GithubController();
     app.use(express.json());
+    app.use(GithubSha256Middleware.verifySignature);
     app.post('/api/github', controller.webhookHandler);
     app.listen(envs.PORT, () => {
         console.log(`Server listening on port ${envs.PORT}`);
